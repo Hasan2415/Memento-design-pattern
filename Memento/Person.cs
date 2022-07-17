d7bcf0fad886d5d43a170aadd13c9e00676d51c5
H.rezaie <Hasan613379>
2022-07-18 00:34:56 +0430

    memento design pattern

---
+++ Memento/Person.cs
@@ -0,0 +1,47 @@
+﻿public class Person
+{
+    private List<Snapshot> _snapshots = new List<Snapshot>();
+    public string Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+
+    private Person(string firstName, string lastName, string email)
+    {
+        Id = Guid.NewGuid().ToString();
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public static Person Create(string firstName, string lastName, string email)
+    {
+        return new Person(firstName, lastName, email);
+    }
+
+    public void Edit(string firstName, string lastName, string email)
+    {
+        _snapshots.Add(CreateSnapshot());
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public void Restore()
+    {
+        var lastPerson = _snapshots.Last();
+        FirstName = lastPerson.FirstName;
+        LastName = lastPerson.LastName;
+        Email = lastPerson.Email;
+    }
+
+    private Snapshot CreateSnapshot()
+    {
+        return new Snapshot
+        {
+            FirstName = FirstName,
+            LastName = LastName,
+            Email = Email
+        };
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Start for create object :");
+var person = Person.Create("Hasan", "rezaei", "hasan@gmail.com");
+
+Console.WriteLine($"first name : {person.FirstName} ; " +
+    $"last name : {person.LastName} ; " +
+    $"email : {person.Email}");
+
+Console.WriteLine();
+Console.WriteLine("Start for edit object : ");
+person.Edit("ali", "yaghobi", "ali@gmail.com");
+
+Console.WriteLine($"first name : {person.FirstName} ; " +
+    $"last name : {person.LastName} ; " +
+    $"email : {person.Email}");
+
+Console.WriteLine();
+Console.WriteLine("Start for restore object :");
+person.Restore();
+
+Console.WriteLine($"first name : {person.FirstName} ; " +
+    $"last name : {person.LastName} ; " +
+    $"email : {person.Email}");
